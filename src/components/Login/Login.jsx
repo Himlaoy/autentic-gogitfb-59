a2b9218ff7900fcd4df592ad563b027b4d4ad9f7
himaloyash <himaloyashis98@gmail.com>
2023-04-15 16:59:47 +0600

    sign in done

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import app from '../../firebase.config';
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth"
+import { getAuth, GithubAuthProvider, GoogleAuthProvider, signInWithPopup } from "firebase/auth"
 
 
 
-const Login = () => {
+const OldLogin = () => {
 
 
     const auth = getAuth(app)
     const gProvider = new GoogleAuthProvider()
+    const gitProvider = new GithubAuthProvider()
 
 
 
@@ -23,12 +24,22 @@ const Login = () => {
         })
     }
 
+    const handleGithub =()=>{
+        signInWithPopup(auth, gitProvider)
+        .then(result=>{
+            console.log(result.user)
+        })
+        .catch(error=>{
+            console.log(error.message)
+        })
+    }
 
 
     return (
-        <div className='d-flex  justify-content-center '>
+        <div className='d-flex justify-content-center '>
             <div>
-                <h2 onClick={loginGoogle} className='text-success text-center mt-5'>Please Login</h2>
+                <button onClick = {loginGoogle} className='text-success text-center mt-5'>Google Login</button>
+                <button onClick = {handleGithub} className='text-success text-center mt-5'>Github Login</button>
                 <form className='p-4 '>
                     <input type="email" required className='mb-3 rounded p-2 ' name="email" id="email" />
                     <br />
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,9 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { createUserWithEmailAndPassword, getAuth } from 'firebase/auth';
+import app from '../../firebase.config';
 
 const Register = () => {
+    const [success, setSuccess] = useState()
+    const [error, setError] = useState()
+
+
+    const auth = getAuth(app)
+
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        const email = event.target.email.value
+        const password = event.target.password.value
+        console.log(event.target.email.value)
+
+        createUserWithEmailAndPassword(auth, email, password)
+            .then(result => {
+                const loggedUser = result.user
+                setSuccess('log in successfully')
+                setError('')
+                console.log(loggedUser)
+            })
+            .catch(error => {
+                console.error(error)
+                setSuccess('')
+                setError(error.message)
+            })
+    }
+
     return (
-        <div>
-            <h2 className='text-center text-success'>Please Register</h2>
+        <div >
+            <h2 className='text-center text-success mt-4'>Please Register</h2>
+            <div className="d-flex align-item-center justify-content-center">
+                <div >
+                    <form onSubmit={handleSubmit}>
+                        <input type="email" required className='p-2 rounded mb-3' name="email" id="email" placeholder='Enter your email' />
+                        <br />
+                        <input type="password" required className='p-2 rounded mb-3' name="password" id="password" placeholder='Enter your password' />
+                        <br />
+                        <input type="submit" value="Register" className='p-2 rounded mb-3' />
+                    </form>
+                    <p className='text-danger'>{error}</p>
+                    <p className='text-success'>{success}</p>
+                </div>
+            </div>
         </div>
     );
 };
